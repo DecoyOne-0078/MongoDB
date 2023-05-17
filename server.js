25bce9607915257998fae7230f2bce2f31e01ea4
Srinath  V V <82709495+DecoyOne-0078@users.noreply.github.com>
2023-05-17 21:36:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-//Server establishment
+// Server establishment
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
-//Connection establishment
+// Connection establishment
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/sample", {
   useNewUrlParser: true,
@@ -24,26 +25,34 @@ db.once("open", () => {
   console.log("Connected to MongoDB");
 });
 
-//Schema
+// Schema
 const userSchema = new mongoose.Schema({
   name: String,
 });
 
 const User = mongoose.model("User", userSchema);
 
-//Get method
+// Get method
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-//Form submission
+// Form submission
 app.post("/submit", async (req, res) => {
   const name = req.body.name;
+
+  // Validate the form data
+  if (!name || name.trim() === "") {
+    return res.status(400).send("Name is required.");
+  }
 
   try {
     const user = new User({ name });
     await user.save();
     res.sendStatus(200);
+
+    // Redirect to the next page upon successful submission
+    res.redirect("/next-page");
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
